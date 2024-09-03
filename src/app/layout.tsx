b4e7397e5f5bb70a6b48a62b85ee87ec683b77bb
Nihir Shah <nihir@demandhelm.com>
2024-09-03 18:24:20 +0530

    Clarity fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+            `,
+          }}
+        />
+        {children}</body>
     </html>
   );
 }
